85541eafc0fad3a5ee988121d8a9ccb3e9e3ee9f
roman <kalibri_grozd@mail.ru>
2022-12-17 20:05:51 +0300

    Did HW6

---
+++ seminar6HW/Program.cs
@@ -0,0 +1,35 @@
+﻿// Домашняя работа 6.
+
+// Задача 1. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+/*
+Console.Write("Input m numbers: ");
+int m = Convert.ToInt32(Console.ReadLine());
+ int count = 0;
+ for (int i = 0; i < m; i++)
+ {
+    Console.Write($"Input number {i + 1}: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n > 0)
+        count++;
+ }
+
+Console.Write($"Positive numbers of M numbers: {count} ");
+*/
+
+// Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+/*
+Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+
+var x = (b2 - b1) / (k1 - k2);
+var y = k1 * x + b1;
+
+Console.Write($"Point of intersection two lines: ({x}, {y}).");
+*/
